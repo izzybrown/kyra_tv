fd9fe7d720b871ef033c8c3fed0f55b1f659bfc7
Izzy Brown <izzybrown@hotmail.co.uk>
2019-01-07 18:40:13 +0000

    all content

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,16 +4,9 @@ require 'open-uri'
 
 class PagesController < ApplicationController
   def home
-    video = []
+    content = []
     url = open("https://www.googleapis.com/youtube/v3/activities?part=snippet,contentDetails&channelId=UCvO6uJUVJQ6SrATfsWR5_aA&key=#{ENV['API_KEY']}&maxResults=50").read
     json = JSON.parse(url)
-    @title = json["items"][0]['snippet']['title']
-    @thumbnail = json["items"][0]['snippet']["thumbnails"]['url']
-    raise
-    # @title = content_array['snippet']['title']
-    # @title.each do |title|
-    #   array << title
-    #   end
-    # raise
+    @items = json['items']
   end
 end
