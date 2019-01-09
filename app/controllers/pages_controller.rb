94743db2decc9aff941c2fdf9e05d3da8a36e3bf
Izzy Brown <izzybrown@hotmail.co.uk>
2019-01-09 10:14:31 +0000

    security

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 
 class PagesController < ApplicationController
   def home
-    url = open("https://www.googleapis.com/youtube/v3/activities?part=snippet,contentDetails&channelId=UCvO6uJUVJQ6SrATfsWR5_aA&key=AIzaSyC0O_IkXyYYUzhItB2M0FpWBLrQuaAFL_U&maxResults=50").read
+    url = open("https://www.googleapis.com/youtube/v3/activities?part=snippet,contentDetails&channelId=UCvO6uJUVJQ6SrATfsWR5_aA&key=#{ENV['API_KEY']}&maxResults=50").read
     json = JSON.parse(url)
     @items = json['items']
     @image_id = json['items'][0]['contentDetails']['upload']['videoId']
